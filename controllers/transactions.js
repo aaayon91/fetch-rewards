072b543198fad9bf71094b67c10cfc52bfb65a01
aaayon91 <aaayon91@gmail.com>
2022-05-09 23:02:35 -0700

    adds sorting to transactions array

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,24 +1,41 @@
+let transactions = []
+// let arrangedTrans = []
+
 module.exports ={
     index,
     // show,
     new: newTransaction,
     create,
     // delete: deleteTodo,
-    // edit,
-    // update
+    // edit
 };
 
 function newTransaction(req, res) {
+    console.log('date', Date.now().toString())
     res.render('transactions/new')
 }
 
 function index(req, res) {
+    // transactions = transactions.sort(function(a,b){
+    //     return new Date(a.timestamp) - new Date(b.timestamp);
+    //   });
+    console.log('Sorted Transactions', transactions)
     res.render('transactions/index', {
-        // transactions: ,
+        transactions,
     })
 }
 
 function create(req, res) {
-    console.log(req.body)
-    
+    // For sorting transactions on submit:
+    let idx = 0;
+    if (transactions.lengt === 0) transactions.push(JSON.parse(JSON.stringify(req.body)));
+    else {
+        for (let i = 0; i < transactions.length; i++) {
+            if (new Date(transactions[i].timestamp) <= new Date(req.body.timestamp)) {
+                idx = i + 1;
+            }
+        }
+        transactions.splice(idx, 0, JSON.parse(JSON.stringify(req.body)));
+    }
+    res.redirect('/transactions')
 }
